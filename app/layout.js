26209241ff8543351e5f7f684c3b5d535b27a9b3
Ozar <87942063+Xantonozar@users.noreply.github.com>
2025-06-29 00:25:08 +0600

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -10,14 +10,48 @@ const exoItalic = localFont({
 });
 
 export const metadata = {
-  title: "AUSTESWC",
-  description:
-    "Ahsanullah University of Science and Technology Environmental and Social Welfare Club is a Environmental and Social Welfare Club",
+  title: {
+    default: 'AUSTESWC || Save People ,Save Environment , Save The Society',
+    template: '%s | AUSTESWC ',
+  },
+  description: 'Save People ,Save Environment , Save The Society',
+  icons: {
+    icon: [
+      { url: '/eswclogo.svg', type: 'image/svg+xml' }
+    ],
+    shortcut: '/eswclogo.svg',
+    apple: '/eswclogo.svg'
+  },
+  openGraph: {
+    title: 'AUSTESWC ||Save People ,Save Environment , Save The Society ',
+    description: '',
+    url: 'https://austeswc.org',
+    siteName: 'AUSTESWC',
+    images: [
+      {
+        url: 'https://austeswc.org/eswclogo.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'AUSTESWC',
+    description: 'Save People ,Save Environment , Save The Society',
+    images: ['https://austeswc.org/eswclogo.png'],
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+    <link rel="icon" href="/eswclogo.png" sizes="any" />
+  </head>
       <body className={exoItalic.variable}>
         <Navbar />
         {children}
